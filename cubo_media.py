10baaec59f1ce5fb5d04b1314918cc9da3ba548f
Gabriel Gomes <105324726+GabrielGomes99@users.noreply.github.com>
2023-08-03 21:29:56 -0300

    Valor do Cubo/Média Geométrica
    Usuário insere dois valores, ele dar o valor do segundo número ao cubo e depois 
    dar a média geométrica dos dois valores.

---
+++ cubo_media.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+num1 = int(input("Digite o primeiro valor inteiro: "))
+num2 = int(input("Digite o segundo valor inteiro: "))
+
+calculo2 = num2 ** 3 
+calculo1 = sqrt (num1 * num2)
+
+print("O cubo do segundo número é:", calculo2)
+print("A média geométrica entre o primeiro e o segundo número é:", calculo1)
