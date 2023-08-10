d81b7b6f834cd20c2ba4d2254aee26c6335d4c01
Gabriel Gomes <105324726+GabrielGomes99@users.noreply.github.com>
2023-08-10 14:50:36 -0300

    Valor de Delta
    Trabalho simples envolvendo aritméticos matemáticos

---
+++ Delta.py
@@ -0,0 +1,9 @@
+import math
+
+aX = float(input("Digite o valor de A: "))
+bX = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+
+delta = bX**2 - 4*aX*C
+
+print(f"O resultado da equacação é = {delta: .2f}")
