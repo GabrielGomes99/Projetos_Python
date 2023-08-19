3ab385a7acdecf31ca4fd82ecb3b51f694f4726d
Gabriel Gomes <105324726+GabrielGomes99@users.noreply.github.com>
2023-08-19 01:59:32 -0300

    Delete Projeto_Ponto/projeto_cad_usuario directory

--- Projeto_Ponto/projeto_cad_usuario/projeto_cad_usuario/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app_cad_usuarios import views
-
-urlpatterns = [
-    # rota, view responsável, nome de referência
-    # usuarios.com
-    path('',views.home,name='home'),
-]
